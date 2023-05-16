cb65e169affa612ad7e53f0549ca9121c50188c3
kapilpratihar <109463318+kapilpratihar@users.noreply.github.com>
2023-05-17 01:51:04 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechSkill</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.youtube.com/@TechSkillsAcademyin"
+            target={"blank"}
+          >
+            Youtube
+          </a>
+          <a href="https://instagram.com/kapilpratihar_kp" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/kapilpratihar" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>TechSkill</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            TechSkill Kids is a cutting-edge educational technology company
+            dedicated to empowering and enhancing the skills of children through
+            innovative technological solutions. Our mission is to equip the
+            younger generation with the essential skills and knowledge needed to
+            thrive in a rapidly evolving digital world. At TechSkill Kids, we
+            recognize the importance of nurturing a child's potential from an
+            early age. We have developed a range of engaging and interactive
+            learning tools and platforms specifically designed to foster
+            creativity, critical thinking, problem-solving, and other essential
+            skills. Our flagship product, the TechSkill Kids Learning System, is
+            an immersive and personalized educational platform that utilizes
+            state-of-the-art technologies such as artificial intelligence,
+            virtual reality, and gamification. This platform offers a diverse
+            collection of educational games, interactive lessons, and hands-on
+            projects, all carefully curated to align with modern educational
+            standards and promote a love for learning.
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
